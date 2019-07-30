2d0e4faab1be66ee94be810dd21558336d1f6a6f
Oleksandr Mormul <omormul.wemyit@gmail.com>
2019-07-30 23:23:22 +0300

    Registered model

--- backend/caesar_cipher/models.py
+++ backend/caesar_cipher/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Cipher(models.Model):
+    shift = models.IntegerField()
+    content_encode = models.CharField(max_length=100)
+    content_decode = models.CharField(max_length=100)
+
+    def __str__(self):
+        "String representation of the Cipher object."
+        return f'Content {self.pk}'
