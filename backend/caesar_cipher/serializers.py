838ca1eda469b8e10e5ba09bb65617a0905d76ea
Oleksandr Mormul <omormul.wemyit@gmail.com>
2019-07-30 23:24:55 +0300

    Added serializers

---
+++ backend/caesar_cipher/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Cipher
+
+
+class CipherSerializer(serializers.ModelSerializer):
+
+    content_encode = serializers.CharField(max_length=100)
+    content_decode = serializers.CharField(max_length=100)
+    shift = serializers.IntegerField()
+
+    class Meta:
+        model = Cipher
+        fields = ('content_encode', 'content_decode', 'shift')
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.views import generic
+from rest_framework.schemas import get_schema_view
+from rest_framework import status, serializers, views
 from django.conf.urls import url, include
+from caesar_cipher import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^$', generic.RedirectView.as_view(url='/api/', permanent=False)),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^api/', csrf_exempt(views.CaesarCipherView.as_view()), name='create')
 ]
