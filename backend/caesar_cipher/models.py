f16751b7037c67f8a15ed90a54f15a310fa6a136
Oleksandr Mormul <omormul.wemyit@gmail.com>
2019-08-02 10:34:47 +0300

    Implementation post request

--- backend/caesar_cipher/models.py
+++ backend/caesar_cipher/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Cipher(models.Model):
     shift = models.IntegerField()
     content_encode = models.CharField(max_length=100)
-    content_decode = models.CharField(max_length=100)
+    content_decode = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         "String representation of the Cipher object."
         return f'Content {self.pk}'
+
+    @classmethod
+    def save_result(cls, text):
+        cipher = cls.objects.order_by('-id').first()
+        cipher.content_decode = text
+        cipher.save()
--- backend/caesar_cipher/serializers.py
+++ backend/caesar_cipher/serializers.py
@@ -5,7 +5,8 @@ from .models import Cipher
 class CipherSerializer(serializers.ModelSerializer):
 
     content_encode = serializers.CharField(max_length=100)
-    content_decode = serializers.CharField(max_length=100)
+    content_decode = serializers.CharField(
+        max_length=100, required=False, allow_blank=True)
     shift = serializers.IntegerField()
 
     class Meta:
