b0b2c9d8b69bd69bea99cc39707fcd2cd5625bf0
Oleksandr Mormul <omormul.wemyit@gmail.com>
2019-08-04 15:22:59 +0300

    decode method

--- backend/caesar_cipher/views.py
+++ backend/caesar_cipher/views.py
@@ -4,6 +4,17 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .serializers import CipherSerializer
 from .models import Cipher
+import enchant
+
+
+def get_cipherletter(new_key, letter):
+    # still need alpha to find letters
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    if letter in alpha:
+        return alpha[new_key]
+    else:
+        return letter
 
 
 def encode(text, shift):
@@ -15,6 +26,18 @@ def encode(text, shift):
             result += chr((ord(char) + shift-65) % 26 + 65)
         else:
             result += chr((ord(char) + shift-97) % 26 + 97)
+    return result
+
+
+def decode(key, message):
+    message = message.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ""
+
+    for letter in message:
+        new_key = (alpha.find(letter) - key) % len(alpha)
+        result = result + get_cipherletter(new_key, letter)
+
     return result
 
 
@@ -31,8 +54,20 @@ class CaesarCipherView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
+        eng_dict = enchant.Dict("en_US")
         data = Cipher.objects.order_by('-id').first()
-        content = data.content_encode
+        if data:
+            content = data.content_encode
+            check = eng_dict.check(content)
+            if check:
+                shift = data.shift
+                result = encode(content, shift)
+                Cipher.save_result(result)
+            else:
+                content = data.content_encode
+                shift = data.shift
+                result = decode(shift, content)
+                Cipher.save_result(result)
         serializer = CipherSerializer(
             data, context={'request': request})
         return Response(serializer.data)
